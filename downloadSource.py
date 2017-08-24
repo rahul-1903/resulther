78215d2db0fb3c4449b68d0b1d09280bf9adfb5f
ashketcham <rahulkumargupta19.03@gmail.com>
2017-08-24 16:20:48 +0530

    Update downloadSource.py

--- downloadSource.py
+++ downloadSource.py
@@ -1,51 +1,85 @@
-import requests, urllib, lxml.html, ctypes, re, sys, time, os
+import lxml.html, sys, time, os
 from bs4 import BeautifulSoup
 from mechanize import Browser
+
 #getting the form attribute
 def select_form(form):
     return form.attrs.get('id',None)=='form1'
+
+#url for downloading the data
 url = "http://61.12.70.61:8084/"
+
+#filename
+filename = "result.txt"
 
 def result(number):
     br = Browser()
     br.open(url)
     br.select_form(predicate=select_form)
-    br['roll'] = str(number)    #inserting the roll number and semester
+    # inserting the roll number and semester
+    br['roll'] = str(number)
     br['sem'] = ['2']
-    resp = br.submit() # submiting and storing the source code in resp
+
+    # submiting and storing the source code in resp
+    resp = br.submit()
+
     soup = BeautifulSoup(resp, "lxml").prettify()
     l = soup.split('\n')
-    #print l[77].strip(), l[72].strip(), l[340].strip()
     msg = l[3].strip()
     if msg != "No such student exists in this database":
+        sgpa1, sgpa2 = l[326].split(), l[333].split()
         reg, name, ygpa = l[77].split(), l[72].split(), l[340].split()
-        return (reg[-1]), " ".join(name[2:]), "".join(ygpa[-1])
-    else :
-        return "Roll {} does not exist".format(number)
-
-compStart, compEnd = 12616001001, 12616001200     #for computer science and engineering
-civilStart, civilEnd = 12616013001, 12616013121   # for civil
-#filename = "file.txt"
-filename = "result.txt"
+        return (reg[-1]), " ".join(name[2:]), "".join(sgpa1[-1]), "".join(sgpa2[-1]), "".join(ygpa[-1])
+    else:
+        return None
+    
 file = open(filename, "w")
-file.write("Roll\t\tName\t\tYGPA\n")
+file.write("%-5s%-12s%-25s%-7s%-6s%-6s\n"%("Rank", "Roll No.", "Name", "Odd", "Even", "YGPA"))
 file.close()
 print("Creating a file...")
 time.sleep(1)
+print("Writing data to the file...")
+'''
 print ("Writing {} data to file...".format(int(sys.argv[2]) - int(sys.argv[1]) + 1))
 time.sleep(1)
 print ("Waiting...")
 if len(sys.argv) < 3:
     print "Usage: python downloadSource.py [InitialRollNo] [FinalRollNo]"
     exit()
-for i in range(int(sys.argv[1]), int(sys.argv[2])+1):
-    file = open(filename, "a")
-    x = result(i)
-    if(len(x) == 3):
-        file.write("{} {} {}\n".format(x[0], x[1], x[2]))
-    else:
-        file.write("Roll No. {} does not exist.\n".format(i))
-    #print x[0], x[1],x[2]
-    file.close()
-print("Finished Writing...")
-print ("Data saved to " + os.getcwd() + "\\" +filename)
+'''
+#creating a list to store details
+l = []
+
+#for i in range(int(sys.argv[1]), int(sys.argv[2])+1):
+def res(a, b):
+    total = b - a + 1
+    for i in range(a, b):
+        time.sleep(0.5)
+        if i>0:
+            perComplete = ((float(i-a)/total)*100)
+            if(perComplete % 5 == 0):
+                print ("Completed {}% ".format(perComplete))
+        x = result(i)
+        if(x != None and len(x) == 5):
+            m = [x[0], str(x[1]), float(x[2]), float(x[3]), float(x[4])]
+            l.append(m)
+    print("Finished Writing...")
+    print ("Data saved to " + os.getcwd() + "\\" +filename)
+
+    #sorting the data in descending order of YGPA
+    l.sort(key=lambda x: x[4], reverse=True)
+
+    rank = 1
+    for i in range(len(l)):
+        file = open(filename, 'a')
+        if (i >= 1 and l[i][4] != l[i-1][4]):
+            rank += 1
+        file.write ("%-4d%-12s%-25s%-7.2f%-7.2f%-7.2f\n"%(rank, l[i][0], l[i][1], l[i][2], l[i][3], l[i][4]))
+        file.close()
+
+# Roll Numbers of the various departments
+compStart, compEnd = 12616001001, 12616001188   #for computer science and engineering
+civilStart, civilEnd = 12616013001, 12616013121   # for civil
+
+#calling res function
+res(compStart, compEnd)
